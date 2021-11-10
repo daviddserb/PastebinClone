37bcc815570c3d9f60611a7efce71c5da1b04cc8
Serb David <83017887+daviddserb@users.noreply.github.com>
2021-11-10 10:34:27 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,8 @@
 const express = require('express');
 const mysql = require('mysql');
 const router = express.Router();
-var dt = require('../public/javascripts/pasteCreatedDate');
-
-console.log("_______TESTARE DATE-FNS_______")
-console.log(new Date());
-let datefns = require('date-fns');
-console.log(datefns.addDays(new Date(), 1));
-
-console.log("_______TESTARE RANDOM_______")
-//format date save in db: "YYYY-MM-DD hh:mm:ss"
-/*var date = new Date(); // DATE IN UTC
-console.log(date);
-console.log("" + new Date()); //se transforma in string
-console.log(new Date().toString()); // Date in local time zone
-console.log("--------------------");
-console.log(new Date().getHours());
-console.log(dt.createdDate());
-console.log("######################");
-console.log(new Date(date + 'UTC')); //DATE IN LOCAL TIME
-console.log(new Date(date + "UTC").toISOString().replace(/T/, ' ').replace(/\..+/, '')); //formatted to be saved in db
-console.log("$$$$$$$$$$$$$$$$$$$");
-console.log(date.toUTCString()); // Date in UTC time zone
-console.log(new Date().toISOString()); // YYYY-MM-DDTHH:mm:ss.sssZ and the time zone is always UTC, as denoted by the suffix Z.
-*/
+const dt = require('../public/javascripts/pasteCreatedDate');
+const datefns = require('date-fns');
 
 //Create the connection to the database
 const connection = mysql.createConnection({
@@ -48,7 +27,6 @@ connection.query('CREATE TABLE IF NOT EXISTS users(' +
   'id INT(255) UNSIGNED AUTO_INCREMENT PRIMARY KEY,' +
   'text VARCHAR(255) NOT NULL,' +
   'dateAdded DATETIME NOT NULL,' +
-  'expireMins VARCHAR(255),' +
   'dateExpired DATETIME' +
   ')', (err) => {
   if (err) {
@@ -58,37 +36,36 @@ connection.query('CREATE TABLE IF NOT EXISTS users(' +
   }
 });
 
-//User page (All the pastes)
+//User's pastes (All the pastes)
 router.get('/', (req, res) => {
   selectDataFromDatabase(res);
 });
 
 //Create Member
 router.post('/', (req, res) => {
-  let expireTimeMinutes = req.body.expireTimeId;
-  console.log("dupa cate minute sa se stearga paste-ul= " + req.body.expireTimeId);
+  let expireTime = req.body.expireTimeName;
+  let inputText = req.body.inputTextName;
 
   //Insert data in the database
-  connection.query("INSERT INTO users(text, dateAdded, expireMins)" +
-    "VALUES ('"+ req.body.inputText +"', '"+ dt.createdDate() +"', '"+ expireTimeMinutes +"')", (err) => {
-      if (err) {
-        throw err;
-      } else {
-        console.log("inserted into the database");
-      }
-    });
+  connection.query("INSERT INTO users(text, dateAdded)" +
+    "VALUES ('"+ inputText +"', '"+ dt.createdDate() +"')", (err) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log("inserted into the database");
+    }
+  });
 
-  if (expireTimeMinutes != "never") {
-    //Update the expired date in the database
-    connection.query(`UPDATE users SET dateExpired = '${calculateExpirationDate(expireTimeMinutes)}' WHERE dateAdded = '${dt.createdDate()}'`, (err) => {
+  if (expireTime != "never") {
+    connection.query(`UPDATE users SET dateExpired = '${calculateExpirationDate(expireTime)}' WHERE dateAdded = '${dt.createdDate()}'`, (err) => { //sa aflu cum sa iau id-ul
       if (err) {
         throw err;
       } else {
-        console.log("dateExpired updated in the database");
+        console.log("database updated");
       }
     });
   }
-  //Select the data from db and show it on a web page
+  //Select data from the database and print it on the web page
   selectDataFromDatabase(res);
 });
 
@@ -102,7 +79,7 @@ connection.query("ALTER EVENT deletePastesAfterTime" +
 
 //connection.query("DROP EVENT deletePastesAfterTime"); //delete event
 
-//Update get (change the text)
+//Update get -> select the text
 router.get('/edit/:id', function(req, res) {
   let userId = req.params.id; //take the user id this way because the line above has /:id
   let sqlSelect = `SELECT * FROM users WHERE id = ${userId}`;
@@ -113,8 +90,8 @@ router.get('/edit/:id', function(req, res) {
       //check if the paste date is expired
       if (data[0].dateExpired < new Date() && data[0].dateExpired != null) {
         let sqlDelete = `DELETE FROM users WHERE id = ${userId}`;
-        let errorHandler = "yes";
-        deletePastes(res, sqlDelete, errorHandler)
+        deletePaste(sqlDelete);
+        res.redirect('/error');
       } else {
         res.render('usersUpdate', {userPaste: data[0], userNr: userId});
       }
@@ -122,12 +99,12 @@ router.get('/edit/:id', function(req, res) {
   });
 });
 
-//Update post (save and post the new text)
+//Update post -> edit the text and save it
 router.post('/edit/:id', function(req, res) {
   let userId = req.params.id;
-  let updateData = req.body.newText;
-  let sql = `UPDATE users SET text = '${updateData}' WHERE id = ${userId}`;
-  connection.query(sql, [updateData, userId], function(err) {
+  let updateText = req.body.newText;
+  let sql = `UPDATE users SET text = '${updateText}' WHERE id = ${userId}`;
+  connection.query(sql, [updateText, userId], function(err) {
     if (err) {
       throw err;
     } else {
@@ -140,8 +117,8 @@ router.post('/edit/:id', function(req, res) {
 router.get('/delete/:id', function(req, res) {
   let userId = req.params.id;
   let sql = `DELETE FROM users WHERE id = ${userId}`;
-  let errorHandler = "no";
-  deletePastes(res, sql, errorHandler)
+  deletePaste(sql);
+  res.redirect('/users');
 });
 
 //Select all the data from the database and print it on the web page
@@ -155,40 +132,32 @@ function selectDataFromDatabase(res) {
   });
 }
 
-function deletePastes(res, sql, errorHandler) {
-  console.log("paste deleted because it's expired");
+function deletePaste(sql) {
   connection.query(sql, function(err) {
     if (err) {
       throw err;
-    } else if (errorHandler == "yes") { //delete paste because it's expired
-      //res.render('error'); //mai trebuie sa lucrez la eroare, sa ii dau mesaj ca paste-ul ii expirat, ceva de genul
-      res.redirect('/error'); //sau las asa ca si aici, cu redirect
-    } else { //delete the paste because the Delete button was clicked
-      res.redirect('/users');
+    } else {
+      console.log("paste deleted");
     }
   });
 }
 
-function calculateExpirationDate(expireMinutes) {
-  let minutes = parseInt(expireMinutes);
-  console.log("se intra in functia calculateExpirationDate()");
-  let expirationDate = new Date();
-  console.log(dt.createdDate());
-  let howManyHoursToAdd = 0;
-  console.log(expirationDate);
-  expirationDate.setMinutes(expirationDate.getMinutes() + minutes);
-  console.log(expirationDate);
-  while (minutes >= 60) { 
-    ++howManyHoursToAdd;
-    if (howManyHoursToAdd % 24 == 0) {
-      howManyHoursToAdd = 0;
-    }
-    minutes -= 60;
+function calculateExpirationDate(expireTime) {
+  let dateInLocalTime = new Date(new Date() + 'UTC');
+  if (expireTime == "1Min") {
+    var expirationDate = datefns.addMinutes(dateInLocalTime, 1);
+  } else if (expireTime == "1H") {
+    var expirationDate = datefns.addHours(dateInLocalTime, 1);
+  } else if (expireTime == "1D") {
+    var expirationDate = datefns.addDays(dateInLocalTime, 1);
+  } else if (expireTime == "1W") {
+    var expirationDate = datefns.addWeeks(dateInLocalTime, 1);
+  } else if (expireTime == "1Mon") {
+    var expirationDate = datefns.addMonths(dateInLocalTime, 1);
+  } else {
+    var expirationDate = datefns.addYears(dateInLocalTime, 1);
   }
-  console.log("howManyHoursToAdd= " + howManyHoursToAdd);
-  let howManyHoursToAddInTotal = howManyHoursToAdd + new Date().getHours();
-  var expirationDateFormatted = expirationDate.toISOString().replace(/T../, ' ' + howManyHoursToAddInTotal).replace(/\..+/, ''); //formate the date to put in the the db
-  console.log(expirationDateFormatted)
+  let expirationDateFormatted = expirationDate.toISOString().replace(/T/, ' ').replace(/\..+/, '');
   return expirationDateFormatted;
 }
 
